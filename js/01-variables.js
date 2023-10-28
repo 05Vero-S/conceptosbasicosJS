1a070ab736fd7dfa9771390abe00639953dc4289
Vero <vdsoria05@gmail.com>
2023-10-28 15:05:41 -0300

    aprendimos declarar una variable let y const, mostrar mensajes en consola, en body html (document.write), y ventanas emergentes.Tambien como usar y cuando parseInt-parseFload y prompt. Agregamos estilo con css.Practica de un ejercicio ejemplo

--- js/01-variables.js
+++ js/01-variables.js
@@ -9,6 +9,7 @@ let anioActual = 2023; //guardamos un valor (2023) dentro nuestra variable (anio
 const url= "https://universe.rollingcodeschool.com/"; 
 
 //mostrar mensajes
+//en consola
 console.log(anioActual); //muestro en consola el valor de mi variable
 console.log("anioActual"); //muestro en consola el texto
 console.log("Año actual: " + anioActual + " hola mundo") //concatenar texto y valor
@@ -19,3 +20,24 @@ console.log(url)
 anioActual = 2023 + 1;
 
 console.log("Año actual: " + anioActual );
+
+//para mostrar mensaje en nuestro body html
+
+document.write("<h1 class='titulo'>Hola mundo " + anioActual + "</h1>")
+document.write("hola mundo <br>");
+document.write("hola mundo");
+
+//ventanas emergentes
+alert("Esto es un ejemplo de una alerta");
+
+//quiero pedir un numero que ingrese el usuario y luego sumarle 5, por ultimo mostrar el resultado de la suma
+const numeroSolicitado = parseInt(prompt("Ingrese un número del 1 al 100")) ; //se crea una ventana emergente. El dato se guarda en la variable numeroSolicitado
+console.log(numeroSolicitado);
+
+console.log("Resultado: "+ (numeroSolicitado + 5)); //resultado 105
+console.log(numeroSolicitado + 5); // tambien 105
+//el prompt acepta tipo de dato text entonces al numero cuando lo ingreso lo pone entre "" (lo hace texto)
+
+//parseInt("numero entero") le saca la comilla al numero
+//parseFload("numero decimal")
+//esto me devuelve number(o sea un numero)
