9fff0f92d27bc6fe7814322ab7e0f4bec10b2b64
Vero <vdsoria05@gmail.com>
2023-10-28 13:10:58 -0300

    declaramos e inicializamos con let y const

---
+++ js/01-variables.js
@@ -0,0 +1,21 @@
+/* Comentario de mulptiles
+lineas*/
+
+// Comentario de unica linea
+
+// Declarar una variable (let-const)
+
+let anioActual = 2023; //guardamos un valor (2023) dentro nuestra variable (anioActual)
+const url= "https://universe.rollingcodeschool.com/"; 
+
+//mostrar mensajes
+console.log(anioActual); //muestro en consola el valor de mi variable
+console.log("anioActual"); //muestro en consola el texto
+console.log("Año actual: " + anioActual + " hola mundo") //concatenar texto y valor
+console.log(url)
+
+
+//modificar el valor de una variable
+anioActual = 2023 + 1;
+
+console.log("Año actual: " + anioActual );
