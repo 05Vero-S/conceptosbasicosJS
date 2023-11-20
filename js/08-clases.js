7fa7e18027350c8843d91016c9af8d5938c78350
Vero <vdsoria05@gmail.com>
2023-11-20 00:52:30 -0300

    declaramos una propiedad privada

--- js/08-clases.js
+++ js/08-clases.js
@@ -2,30 +2,29 @@
 //diseño un molde para crear diferentes objetos
 
 class VideoJuego{
+    #etiquetas; //propiedad privada
     constructor(tituloParam, generoParam, precioParam, etiquetasParam, anioLanzamientoParam, desarrolladorParam){
         this._titulo = tituloParam;
         this._precio = precioParam;
         this.genero = generoParam;
-        this.etiquetas = etiquetasParam;
+        this.#etiquetas = etiquetasParam;
         this.anioLanzamiento = anioLanzamientoParam;
         this.desarrollador = desarrolladorParam;
         //propiedad por defecto
         this.publicado = false;
     }
-}
-
-//declaracion de propiedades computadas get y set (para cada propiedad crear un get(obtener) y set(modificar))
+    //declaracion de propiedades computadas get y set (para cada propiedad crear un get(obtener) y set(modificar))
 //GET nos va a dar el dato que nosotros queremos consultar
-get titulo (){
+get titulo(){
     return this._titulo;
 }
 
-get precio (){
+ get precio(){
     return this._precio;
 }
 
 //SET nos va a ayudar a modificar un valor (me invento un parametro)
-set titullo (nuevoTitulo){
+set titulo (nuevoTitulo){
     if(nuevoTitulo !== ''){
         this._titulo = nuevoTitulo;
     }else{
@@ -33,7 +32,6 @@ set titullo (nuevoTitulo){
     }
     
 }
-
 //aqui declaro mis metodos
 mostrarDatos(){
     document.write(`<ul>
@@ -43,3 +41,15 @@ mostrarDatos(){
     <li>Desarrollador: ${this.desarrollador} </li>
     </ul>`)
 }
+
+}
+
+
+//crear o instanciar un objeto
+
+const minecraft = new VideoJuego('Minecraft','sandbox', 30, ['cubitos','granja','mundo abierto', 'creeper'], 2008, 'Mojang');
+
+minecraft.mostrarDatos();
+
+document.write(`<p>Titulo del juego: ${minecraft.titulo} , precio: $${minecraft.precio} USD </p>`);
+document.write(`<p>Etiquetas: ${minecraft.etiquetas} </p>`);
