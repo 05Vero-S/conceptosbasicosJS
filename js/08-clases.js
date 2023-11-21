73668bcfd4d73aab6f780787814ece6dff112b74
Vero <vdsoria05@gmail.com>
2023-11-20 21:50:49 -0300

    agregamos un ejemplo de herencia y polimofirmo

--- js/08-clases.js
+++ js/08-clases.js
@@ -44,7 +44,54 @@ mostrarDatos(){
 
 }
 
+//HERENCIA, aqui se hereda todo(propiedades y todos los metodos)
 
+class JuegoDeSupervivencia extends VideoJuego{
+    #protagonista;
+    #antagonista;
+    constructor(tituloParam, generoParam, precioParam, etiquetasParam, anioLanzamientoParam, desarrolladorParam, protagonistaParam, antagonistaParam){
+        //invocar al constructor de la clase padre
+        super(tituloParam, generoParam, precioParam, etiquetasParam, anioLanzamientoParam, desarrolladorParam);
+        this.#protagonista = protagonistaParam;
+        this.#antagonista = antagonistaParam;
+    } 
+
+    get protagonista(){
+        return this.#protagonista;
+    }
+
+    set protagonista(nuevoProtagonista){
+        this.#protagonista = nuevoProtagonista
+
+    }
+    get antagonista(){
+        return this.#antagonista;
+    }
+
+    set antagonista(nuevoAntagonista){
+        this.#antagonista = nuevoAntagonista
+
+    }
+
+    //metodos
+    mostrarInfoExtra(){
+        document.write(`<p>Aqui mostraria todo el detalle de los juegos de supervivencia </p>`);
+    }
+
+    mostrarDatos(){
+        super.mostrarDatos();
+        document.write(`<h2>Informacion extra </h2>
+        <ul>
+        <li>Protaginista: ${this.protagonista} </li>  
+        <li>Antaginista: ${this.antagonista} </li>
+        
+        </ul>`);
+    }
+
+
+}
+
+//puedo agregar los ultimos li directamente copiando todo lo que puse en mostrar datos y queda todo en una lista.
 //crear o instanciar un objeto
 
 const minecraft = new VideoJuego('Minecraft','sandbox', 30, ['cubitos','granja','mundo abierto', 'creeper'], 2008, 'Mojang');
@@ -53,3 +100,9 @@ minecraft.mostrarDatos();
 
 document.write(`<p>Titulo del juego: ${minecraft.titulo} , precio: $${minecraft.precio} USD </p>`);
 document.write(`<p>Etiquetas: ${minecraft.etiquetas} </p>`);
+
+
+const rust = new JuegoDeSupervivencia('Rust','supervivencia',30,['multijugador','realista','pvp','pve'],2016,'Alguien','Personaje principal','El resto');
+
+console.log(rust)
+rust.mostrarDatos();
